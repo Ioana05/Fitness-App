5700c6dfe9445a05947b75c0ee1a8b1290f283b3
Mihai Ocnaru <mihai.ocnaru@mready.net>
2025-01-09 15:19:40 +0200

    fixed exercise type serialization bug

--- graphql/types/exercise/exerciseDifficultyEnumType.js
+++ graphql/types/exercise/exerciseDifficultyEnumType.js
@@ -1,14 +1,14 @@
 import { GraphQLEnumType } from "graphql";
 
 const exerciseDifficultyEnumType = new GraphQLEnumType({
-    name: "exerciseDifficultyEnum",
-    values: {
-        1: { value: "1" },
-        2: { value: "2" },
-        3: { value: "3" },
-        4: { value: "4" },
-        5: { value: "5" }
-    },
+  name: "exerciseDifficultyEnum",
+  values: {
+    LEVEL_1: { value: "1" },
+    LEVEL_2: { value: "2" },
+    LEVEL_3: { value: "3" },
+    LEVEL_4: { value: "4" },
+    LEVEL_5: { value: "5" },
+  },
 });
 
 export default exerciseDifficultyEnumType;
--- graphql/types/exercise/exerciseType.js
+++ graphql/types/exercise/exerciseType.js
@@ -3,16 +3,22 @@ import {
   GraphQLInt,
   GraphQLString,
 } from "graphql";
-import exerciseDifficultyEnumType from "./exerciseDifficultyEnumType";
+import exerciseDifficultyEnumType from "./exerciseDifficultyEnumType.js";
 
 const exerciseType = new GraphQLObjectType({
   name: "Exercise",
   fields: {
     id: { type: GraphQLInt },
     name: { type: GraphQLString },
-    muscleGroup: { type: GraphQLString },
-    difficultyLevel: { type: exerciseDifficultyEnumType },
-    instructions: { type: GraphQLString }
+    muscleGroup: { 
+      type: GraphQLString,
+      resolve: (exercise) => exercise.muscle_group,
+     },
+    difficultyLevel: {
+      type: exerciseDifficultyEnumType,
+      resolve: (exercise) => exercise.difficulty_level,
+    },
+    instructions: { type: GraphQLString },
   },
 });
 
